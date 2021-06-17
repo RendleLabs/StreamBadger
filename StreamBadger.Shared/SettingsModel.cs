c08ba349e0c926e52d2da0f6d68b1fd892babbf3
Mark Rendle <mark@markrendle.net>
2021-06-17 20:40:15 +0100

    Settings page WIP

---
+++ StreamBadger.Shared/SettingsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StreamBadger.Models
+{
+    public class SettingsModel
+    {
+        public string Port { get; set; } = "25293";
+        public string ObsWebSocketsPort { get; set; }
+        public string ObsWebSocketsPassword { get; set; }
+    }
+}
---
+++ StreamBadger.Shared/SettingsStore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using StreamBadger.Models;
+
+namespace StreamBadger.Shared
+{
+    public class SettingsStore
+    {
+        private readonly string _directory;
+        private readonly string _filePath;
+        public SettingsStore()
+        {
+            var userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            _directory = Path.Combine(userProfile, ".streambadger");
+            _filePath = Path.Combine(_directory, "config.json");
+        }
+
+        public async Task<SettingsModel> LoadAsync()
+        {
+            if (!File.Exists(_filePath)) return new SettingsModel();
+            using var reader = File.OpenText(_filePath);
+            var json = await reader.ReadToEndAsync();
+            var model = JsonSerializer.Deserialize<SettingsModel>(json);
+            return model ?? new SettingsModel();
+        }
+
+        public async Task SaveAsync(SettingsModel settings)
+        {
+            if (!Directory.Exists(_directory))
+            {
+                Directory.CreateDirectory(_directory);
+            }
+
+            var json = JsonSerializer.Serialize(settings);
+            await using var writer = File.CreateText(_filePath);
+            await writer.WriteAsync(json);
+        }
+    }
+}
